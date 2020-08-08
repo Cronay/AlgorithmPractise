17a10a3ef984c9d9d86a50495ce31ddfde600b57
Cronay <inf@yannic-borgfeld.de>
2020-08-08 11:51:24 +0200

    First Test for `longestCommonPrefix`

---
+++ AlgorithmPractiseTests/LongestCommonPrefixTests.swift
@@ -0,0 +1,29 @@
+//
+//  LongestCommonPrefixTests.swift
+//  AlgorithmPractiseTests
+//
+//  Created by Cronay on 08.08.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import XCTest
+
+extension Array where Element == String {
+
+    /// If the list of strings is empty return a empty string.
+    func longestCommonPrefix() -> String {
+        return ""
+    }
+}
+
+class LongestCommonPrefixTests: XCTestCase {
+
+    func test_emptyList() {
+        let list = [String]()
+
+        let result = list.longestCommonPrefix()
+
+        XCTAssertEqual(result, "")
+    }
+
+}
