f3db2291e8603e1359a9124151b3ceca45c81aee
Cronay <inf@yannic-borgfeld.de>
2020-08-08 15:11:38 +0200

    Move code from test target to production.

--- AlgorithmPractiseTests/LongestCommonPrefixTests.swift
+++ AlgorithmPractiseTests/LongestCommonPrefixTests.swift
@@ -8,41 +8,6 @@
 
 import XCTest
 
-extension Array where Element == String {
-
-    /// If the list of strings is empty return a empty string otherwise return the longest common prefix of all elements.
-    func longestCommonPrefix() -> String {
-        if self.isEmpty {
-            return ""
-        }
-        if self.count == 1 {
-            return self.first!
-        }
-
-        return findPrefixOfAllElements()
-    }
-
-    private func findPrefixOfAllElements() -> String {
-        var currentPrefix = self[0].prefix(self[0].count)
-        for item in self {
-            let itemsPrefix = item.prefix(currentPrefix.count)
-            if itemsPrefix != currentPrefix {
-                currentPrefix = findCommonPrefix(String(itemsPrefix), String(currentPrefix))
-            }
-        }
-        return String(currentPrefix)
-    }
-
-    private func findCommonPrefix(_ first: String, _ second: String) -> Substring {
-        for i in (0...first.count).reversed() {
-            if first.prefix(i) == second.prefix(i) {
-                return first.prefix(i)
-            }
-        }
-        return ""
-    }
-}
-
 class LongestCommonPrefixTests: XCTestCase {
 
     func test_emptyList() {
